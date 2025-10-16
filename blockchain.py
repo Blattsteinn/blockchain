c0b6ae54894071e0a1c2e7c665f2eee835486b99
Blattsteinn <arnas.lukosius@outlook.com>
2025-10-16 21:23:09 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-from idlelib.pyparse import trans
+
 
 import hash
 
@@ -24,34 +24,52 @@ class User:
         self.__private_key = get_private_key()
         self.public_key = hash.hash_function(self.__private_key) #for simplicity
 
-        self.balance = random.randint(100,1000000)
+        self.balance = random.randrange(100, 1_000_000, 5)
 
     def add(self, _amount):
         self.balance += _amount
 
 
-
 #Account model, switch to UTXO later
 class Transaction:
-
-    def __init__(self, sender_key: str, recipient_key: str, _amount: float):
-        self.transaction_id = hash.hash_function(sender_key + recipient_key + str(amount))
-        self.sender = sender_key
+    def __init__(self, _sender_key: str, recipient_key: str, _amount: int):
+        self.transaction_id = hash.hash_function(_sender_key + recipient_key + str(_amount))
+        self.sender = _sender_key
         self.receiver = recipient_key
         self.amount = _amount
 
 class Block:
-    def __init__(self, previous_hash, transactions):
-        self.timestamp = datetime.datetime.now()
+    def __init__(self, previous_hash, _transactions):
 
+        # Block Header
         self.previous_block_hash = previous_hash
-        self.transaction_list = transactions
-        self.block_hash = None # Here to hash transactions
-        self.merkel_root_hash = None  #
-
+        self.timestamp = datetime.datetime.now()
         self.version = "v0.1"
-        self.nonce = 0 #???
-        self.difficulty_target = 1#???
+        self.merkel_root_hash = hash.hash_function(".".join(_.transaction_id for _ in _transactions)) # Just hash all transactions for now
+        self.nonce = 0
+        self.difficulty_target = 3
+
+        self.block_hash = None
+        self.mined = False
+
+    def mine_block(self):
+        if self.mined:
+            print("Is already mined")
+            return
+
+        hash_start = "0" * self.difficulty_target
+
+        header = f"{self.version}{self.timestamp}{self.previous_block_hash}{self.merkel_root_hash}{self.difficulty_target}"
+
+        while True:
+            current_guess = header + str(self.nonce)
+            self.block_hash = hash.hash_function(current_guess)
+
+            if self.block_hash.startswith(hash_start):
+                self.mined = True
+                break
+            else:
+                self.nonce +=1
 
 class Blockchain:
     def __init__(self):
@@ -60,17 +78,47 @@ class Blockchain:
 if __name__ == "__main__":
 
     # 1) Create 1000 users
-    users = [User() for _ in range(1000)]
+    users = [User() for _ in range(25)]
+    users_dict = {user.public_key: user for user in users}
+    print("Created 1000 users")
 
     # 2) Generate 10,000 transaction
     transactions = []
     for _ in range(10_000):
-        sender = random.choice(users)
-        receiver = random.choice(users)
+        while True:
+            sender = random.choice(users)
+            receiver = random.choice(users)
 
-        amount = random.randint(0, sender.balance)
-        transactions = Transaction(sender, receiver, amount)
-        sender.add(amount)
-        receiver.add(-amount)
+            if sender != receiver:
+                amount = random.randint(1, 20) * 5
 
-    # 3) Creating a block with 100 random transactions
+                if sender.balance >= amount:
+                    break
+
+        transactions.append(Transaction(sender.public_key, receiver.public_key, amount))
+        # sender.add(-amount)
+        # receiver.add(amount)
+    print("Created 10.000 transactions")
+
+    # 3) Choosing 100 random transactions
+    random_transactions = []
+    for _ in range(100):
+        transaction = random.choice(transactions)
+        random_transactions.append(transaction)
+    print("Picked 100 random transactions")
+
+    # 4) Mine the block
+    block = Block(previous_hash="", _transactions = random_transactions)
+    block.mine_block()
+    print("Mined a block")
+
+
+    # 5) Confirm the block and add it to the blockchain
+    transactions = [trx for trx in transactions if trx not in random_transactions]
+    for trx in random_transactions:
+        users_dict[trx.sender].add(-trx.amount)
+        users_dict[trx.receiver].add(trx.amount)
+    random_transactions = []
+
+    # ADD IT TO THE BLOCKCHAIN
+    print("Confirmed a block & added transactions")
